3e185d93b662cf6d534cbc747e1a2bc8a0a79f6a
Anish Kumar <anishsingh0305@outlook.com>
2025-02-12 21:31:06 +0530

    added the Basic Navbar

--- SapnoKiSairUI/src/app/app.component.ts
+++ SapnoKiSairUI/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationTabsComponent } from "./navigation-tabs/navigation-tabs/navigation-tabs.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationTabsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- SapnoKiSairUI/src/app/app.routes.ts
+++ SapnoKiSairUI/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//     { path: 'home', component: HomeComponent },
+//   { path: 'about-us', component: AboutUsComponent },
+//   {
+//     path: 'tour', component: TourComponent, children: [
+//       { path: 'chair-dham-yatra', component: ChairDhamYatraComponent },
+//       { path: 'beach', component: BeachComponent }
+//     ]
+//   },
+//   { path: 'contact-us', component: ContactUsComponent },
+//   { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
